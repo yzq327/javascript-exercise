202d77e7ca81ac8667ec2ccc81fec82c572f2e3f
yzq <you@example.com>
2020-09-21 20:59:05 +0800

    [yzq-921] 1-strig

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,12 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  const tempString = '粤A';
+  let count = 0;
+  for (var i in collection) {
+    let temp = collection[i].slice(0, 2);
+    if (temp === tempString) {
+      count += 1;
+    }
+  }
+  return count;
 }
--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -7,4 +7,8 @@ function getScore(id) {
 
 export default function getScoreOutput(person) {
   // TODO 8: 在这里写实现代码
+  const studentId = person.id;
+  const score = getScore(studentId);
+  const result = `你好，${person.lastName}${person.firstName}！你的考试成绩为${score}分`;
+  return result;
 }
