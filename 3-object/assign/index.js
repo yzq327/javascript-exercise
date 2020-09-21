dc88864d18bec97e84c8a5654e05ba2267909472
yzq <you@example.com>
2020-09-21 22:20:49 +0800

    [yzq-0921] 3-object

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,8 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  source.properties.status = 'processed';
+  const obj1 = { serialNumber: '12345' };
+  let newObj = {};
+  Object.assign(newObj, obj1, source);
+  return newObj;
 }
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,5 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let rsult = parseInt(source.A) + parseInt(source.B) + parseInt(source.C);
+  return rsult;
 }
