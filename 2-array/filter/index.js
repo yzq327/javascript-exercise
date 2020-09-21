646526022c2f7c41ba6cc863c9e5873775104605
yzq <you@example.com>
2020-09-21 21:59:40 +0800

    [yzq-0921 2-array]

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,21 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const result = [];
+  let index = 0;
+  for (const i in collection) {
+    if (collection[i] % 3 === 0) {
+      result[index] = collection[i];
+      index += 1;
+    }
+  }
+  return result;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return collection.filter(function(item, index, self) {
+    return self.indexOf(item) === index;
+  });
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,6 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  collection.splice(0, 5, 'a', 'b', 'c', 'd', 'e');
+  console.log(collection);
+  return collection;
 }
